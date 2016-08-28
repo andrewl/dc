bb9535d3c4e75b3c97fd7bbf596df9818b520c39
Andrew Larcombe <andrew@andrewl.net>
2016-08-28 21:00:36 +0100

    added user as part of build script

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -2,14 +2,10 @@
 set -x
 
 DRUPAL_ROOT=/var/www/site/docroot
-DBUSER=vlad
-DBPASS=wibble
-DBHOST=127.0.0.1
-DBNAME=vladdb
 SITENAME="DDC Publishing"
 SITEMAIL=andrew@andrewl.net
 
-drush -r $DRUPAL_ROOT si standard --db-url=mysql://$DBUSER:$DBPASS@$DBHOST/$DBNAME --site-name=$SITENAME --site-mail=$SITEMAIL -y
+drush -r $DRUPAL_ROOT si standard --site-name=$SITENAME --site-mail=$SITEMAIL -y
 
 
 drush -r $DRUPAL_ROOT en master -y
@@ -22,3 +18,4 @@ drush -r $DRUPAL_ROOT gent tags 7 -y
 drush -r $DRUPAL_ROOT genc --types=author 10 0 -y
 drush -r $DRUPAL_ROOT genc --types=article 500 0 -y
 drush -r $DRUPAL_ROOT eval 'node_access_rebuild();'
+drush -r $DRUPAL_ROOT ucrt ronald --password="password"
