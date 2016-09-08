f6e2e621a494d5b92096044d1bde305ec58e3faa
Andrew Larcombe <andrew@andrewl.net>
2016-09-08 19:00:21 +0100

    moved building to script

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -x
 
-DRUPAL_ROOT=/var/www/site/docroot
-SITENAME="DDC Publishing"
-SITEMAIL=andrew@andrewl.net
 
-drush -r $DRUPAL_ROOT si standard --site-name=$SITENAME --site-mail=$SITEMAIL -y
+sudo chmod +w $DRUPAL_ROOT/sites/default
+drush -r $DRUPAL_ROOT si standard --db-url="mysql://$DB_USERNAME:$DB_PASSWORD@127.0.0.1/$DATABASE" --site-name=$SITENAME --site-mail=$SITEMAIL -y
 
-
+sudo chmod +w $DRUPAL_ROOT/sites/default/settings.php
+echo "require_once 'sites/all/conf/master.settings.inc';" >> $DRUPAL_ROOT/sites/default/settings.php
+sudo chmod -w $DRUPAL_ROOT/sites/default/settings.php
 drush -r $DRUPAL_ROOT en master -y
 drush -r $DRUPAL_ROOT master-ensure-modules --scope=local -y 
 drush -r $DRUPAL_ROOT en ddc_theme -y
