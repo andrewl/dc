444e75b876fa139d4a4546b7334fd3778fec477c
Andrew Larcombe <andrew@andrewl.net>
2016-09-12 21:04:28 +0100

    module hunting

---
+++ docroot/sites/all/modules/custom/ddc_author/src/Author/AuthorWrapper.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @file
+ * Wrapper class for Authors.
+ *
+ * This enables us to wrap functionality associated with authors 
+ * into a single class.
+ */
+namespace Drupal\ddc_author\Author;
+
+use \EntityDrupalWrapper;
+
+class AuthorWrapper extends EntityDrupalWrapper {
+
+  /**
+   * Creates the AuthorWrapper from a nid.
+   *
+   * @param int $nid
+   *   nid of the author. 
+   */
+  public function __construct($nid) {
+    parent::__construct('node', $nid);
+  }
+
+  /**
+   * Get a list of article nids that the Author has written.
+   *
+   * @return int[]
+   *   An array of article titles keyed by nid of articles that this author has written.
+   */
+  public function getArticles() {
+    $articles = array();
+
+    $query = new \EntityFieldQuery();
+    $query->entityCondition('entity_type', 'node')
+          ->entityCondition('bundle', 'article') 
+          ->fieldCondition('field_author', 'target_id', $this->id, '=');
+    $result = $query->execute();
+
+    if (isset($result['node']) && is_array($result['node'])) {
+      foreach (node_load_multiple(array_keys($result['node'])) as $nid => $node) {
+        $articles[$nid] = $node->title;
+      }
+    }
+
+    return $articles;
+  }
+}
